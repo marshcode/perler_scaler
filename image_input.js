c41a9ccfdbd51870be6603d56eb5b85d9decb115
Dave <david.the.marsh@gmail.com>
2025-09-16 13:01:03 -0400

    that's better

--- image_input.js
+++ image_input.js
@@ -1,32 +1,3 @@
-            function drawpattern(ev) {
-                var canvas = document.getElementById('canvas')
-
-                var ctx = canvas.getContext('2d'),
-                    img = new Image(),
-                    f = document.getElementById("uploadpattern").files[0],
-                    url = window.URL || window.webkitURL,
-                    src = url.createObjectURL(f);
-
-                img.src = src;
-
-                img.onload = function() {
-                    resize_canvas('canvas', this.width, this.height);
-                    // consider bringing this down for larger images
-                    ctx.drawImage(img, 0, 0);
-                    copy_canvas(canvas, backCanvas, 0, 0)
-                    url.revokeObjectURL(src);
-
-                    // this lets us use other formats in the future
-                    const image_grid_color = function(x, y){
-                        const canvas = document.getElementById("canvas");
-                        const ctx = canvas.getContext("2d");
-                        return get_color_at_grid_point(x, y, ctx);
-                    }
-
-                    set_scale_options(canvas.width / GRID_SIZE, canvas.height / GRID_SIZE);
-                    set_scale_onclick(image_grid_color);
-                }
-            }
 
             function drawsprite(ev) {
                 var canvas = document.getElementById('canvas')
@@ -57,8 +28,9 @@
 
                         return pixel_data;
                     }
-                    set_scale_options(canvas.width+1, canvas.height+1);
-                    set_scale_onclick(image_grid_color);
+
+                    let onclick = set_scale_onclick(image_grid_color, canvas.width+1, canvas.height+1);
+                    onclick();
                 }
                 return f.name
             }
