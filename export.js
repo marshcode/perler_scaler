174e84210c5fac80f8e39c7326e3322ac46ad4ec
Dave <david.the.marsh@gmail.com>
2025-09-22 19:47:56 -0400

    refactoring

--- export.js
+++ export.js
@@ -36,3 +36,37 @@ var color_map_to_data_list = function(color_map, color_legend){
     })
     return data_list;
 }
+
+
+var png_export = function(canvas_list, canvas_target, color_legend){
+
+    source_width = canvas_list[0].width;
+    source_height = canvas_list[0].height;
+
+    canvas_target.width = source_width + (GRID_SIZE * 20);
+    canvas_target.height = source_height;
+    var to_canvas_context = canvas_target.getContext('2d');
+
+    to_canvas_context.fillStyle = document.getElementById("PNG_BACKGROUND").value
+    to_canvas_context.fillRect(0, 0, source_width, source_height)
+    to_canvas_context.fillStyle = "white"
+    to_canvas_context.fillRect(source_width, 0, source_width + (GRID_SIZE * 20), source_height)
+
+    canvas_list.forEach(function(source_canvas){
+        to_canvas_context.drawImage(source_canvas, 0, 0);
+    })    
+
+    let grid_y_idx = 0;
+    COLOR_LEGEND.get_color_legend().forEach(function(_, key, _){
+        const color_index = COLOR_LEGEND.get_color_index(key);
+        const [r, g, b, a_] = COLOR_LEGEND.parse_key(key)
+
+        var coords = grid_coords_from_canvas(source_width, 0);
+        grid_y_idx += 2;
+        draw_rect(coords.grid_x, grid_y_idx, [r, g, b, 255], to_canvas_context)
+        const style = is_light(r, g, b) ? "white" : "black";
+        draw_styled_text(coords.grid_x, grid_y_idx, ""+color_index,  style, to_canvas_context);
+
+    });
+
+}
